5edc1207497463f4e86c2146d751ee8228490dec
Kaz <zhenglevi69@gmail.com>
2025-11-27 22:18:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -99,8 +99,8 @@ async function loadSongs() {
                 let vol = 0;
                 const fadeIn = setInterval(() => {
                     vol += 0.05;
-                    if(vol >= 0.8) {
-                        vol = 0.8;
+                    if(vol >= 1) {
+                        vol = 1;
                         clearInterval(fadeIn);
                     }
                     audio.volume = vol;
