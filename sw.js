ffa2118348039c935e65a1688139820db832b0d9
zhenglevi69 <zhenglevi69@gmail.com>
2026-09-16 13:07:21 +0800

    Site updated: 2026-09-16 13:07:21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1789535039599";const preCache = ["/images/taichi.png","/images/shinobg.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1789535237724";const preCache = ["/images/taichi.png","/images/shinobg.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
